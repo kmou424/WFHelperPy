23fe1b84e696563b7ad276c763d2d696dc638b11
kmou424 <me@kmou424.moe>
2022-03-05 17:32:32 +0800

    [add] install: Add prebuilt wheel to avoid compilation of some package

--- install.py
+++ install.py
@@ -7,8 +7,12 @@ from lib.file import FileCtrl
 
 PATH_DELIMITER = FileCtrl.getPathDelimiter()
 
+WIN_WHEEL_LIST = ['prebuilt/wheel/python_Levenshtein-0.12.2-cp37-cp37m-win_amd64.whl']
+
 TEMPLATE_PATH = 'template'
 TEMPLATE_MIRROR = "https://gitee.com/kmou424/wfhelperpy_template"
+PREBUILT_PATH = "prebuilt"
+PREBUILT_MIRROR = "https://gitee.com/kmou424/wfhelperpy_prebuilt"
 VENV_PATH = 'tools'
 VENV_BIN_DIR = 'tools/Scripts'
 VENV_PIP_PATH = 'tools/Scripts/pip.exe'
@@ -18,6 +22,11 @@ if 'linux' in sys.platform or 'mac' in sys.platform:
     VENV_BIN_DIR = VENV_BIN_DIR.replace('Scripts', 'bin')
     VENV_PIP_PATH = VENV_PIP_PATH.replace('.exe', '').replace('Scripts', 'bin')
     VENV_PYTHON_PATH = VENV_PYTHON_PATH.replace('.exe', '').replace('Scripts', 'bin')
+
+
+def clonePrebuiltRepo():
+    Logger.displayLog("正在拉取Prebuilt文件")
+    os.system("git clone {repo_url} {prebuilt_path}".format(repo_url=PREBUILT_MIRROR, prebuilt_path=PREBUILT_PATH))
 
 
 def cloneTemplateRepo():
@@ -54,6 +63,11 @@ def removeOldRunScript():
 
 def installRequirements():
     Logger.displayLog("检查并安装依赖")
+    os.system('{pip_path} install wheel'.format(pip_path=VENV_PIP_PATH.replace('/', PATH_DELIMITER)))
+    if 'win' in sys.platform:
+        Logger.displayLog("安装预编译wheel")
+        for whl in WIN_WHEEL_LIST:
+            os.system('{pip_path} install {whl}'.format(pip_path=VENV_PIP_PATH.replace('/', PATH_DELIMITER), whl=whl))
     _ret = os.system(
         '{pip_path} install -r requirements.txt'.format(pip_path=VENV_PIP_PATH.replace('/', PATH_DELIMITER)))
     removeOldRunScript()
@@ -64,12 +78,12 @@ def installRequirements():
         Logger.displayLog("安装依赖失败", Logger.LOG_LEVEL_ERROR, quit_code=_ret)
 
 
-Logger.displayLog("正在检查模板文件")
 # 控制台返回值
 ret = 0
+Logger.displayLog("正在检查模板文件")
 if Path(TEMPLATE_PATH).exists():
     Logger.displayLog("检测到旧的模板文件")
-    Logger.displayLog("更新上游文件")
+    Logger.displayLog("更新上游...")
     ret = os.system('git -C {template_path} pull'.format(template_path=TEMPLATE_PATH))
     if ret != 0:
         Logger.displayLog("更新上游失败", Logger.LOG_LEVEL_WARNING)
@@ -78,6 +92,19 @@ if Path(TEMPLATE_PATH).exists():
         cloneTemplateRepo()
 else:
     cloneTemplateRepo()
+
+Logger.displayLog("正在检查Prebuilt文件")
+if Path(PREBUILT_PATH).exists():
+    Logger.displayLog("检测到旧的Prebuilt文件")
+    Logger.displayLog("更新上游...")
+    ret = os.system('git -C {prebuilt_path} pull'.format(prebuilt_path=PREBUILT_PATH))
+    if ret != 0:
+        Logger.displayLog("更新上游失败", Logger.LOG_LEVEL_WARNING)
+        Logger.displayLog("尝试删除现有Prebuilt文件并重新拉取")
+        FileCtrl.rmtree(PREBUILT_PATH)
+        clonePrebuiltRepo()
+else:
+    clonePrebuiltRepo()
 
 Logger.displayLog("正在检查虚拟环境")
 if Path(VENV_PATH).exists():
